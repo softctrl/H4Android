96dcb89ed2acaa6290b9b91eb43eb564d37c42ef
carlostimoshenkorodrigueslopes@gmail.com <carlostimoshenkorodrigueslopes@gmail.com@8a653711-6ef5-5ea1-44ff-28f1be3985c9>
2011-11-02 23:21:59 +0000

    refactoring in framework

---
+++ h4android/src/br/com/softctrl/h4android/orm/engine/i/IModelORMDDL.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2011 Carlos Timoshenko Rodrigues Lopes
+ * 
+ * I thank GOD for the insatiable desire to acquire knowledge that was given to
+ * me. The search for knowledge must be one of our main purposes as human beings.
+ * I sincerely hope that this simple tool is in any way useful to the community
+ * in general.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package br.com.softctrl.h4android.orm.engine.i;
+
+/**
+ * @author <a href="mailto:carlostimoshenkorodrigueslopes@gmail.com">Timoshenko</a>.
+ * @version $Revision: 0.0.0.1 $
+ */
+public interface IModelORMDDL {
+
+	public String createSQL();
+	
+	public String dropSQL();
+
+	//public String insertSQL();
+
+	//public String updateSQL();
+
+	//public String deleteSQL();
+
+	//public String selectSQL();
+
+}
