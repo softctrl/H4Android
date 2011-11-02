04f434ee118dcc9a8073ae01aec2796a1a7f5f24
carlostimoshenkorodrigueslopes@gmail.com <carlostimoshenkorodrigueslopes@gmail.com@8a653711-6ef5-5ea1-44ff-28f1be3985c9>
2011-11-02 23:25:41 +0000

    exception for intern control

---
+++ trunk/h4android/src/br/com/softctrl/h4android/orm/exception/SelectAllQueryException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.softctrl.h4android.orm.exception;
+
+/**
+ * @author timoshenko
+ *
+ */
+public class SelectAllQueryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1894678566014942085L;
+	
+	public SelectAllQueryException(String message) {
+		super(message);
+	}
+
+}
