dc88c4f720317753abe18220e5bd3eb517f56e56
carlostimoshenkorodrigueslopes@gmail.com <carlostimoshenkorodrigueslopes@gmail.com@8a653711-6ef5-5ea1-44ff-28f1be3985c9>
2011-11-02 23:25:27 +0000

    exception for intern control

---
+++ trunk/h4android/src/br/com/softctrl/h4android/orm/exception/MethodDeprecatedInException.java
@@ -0,0 +1,14 @@
+package br.com.softctrl.h4android.orm.exception;
+
+public class MethodDeprecatedInException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7566088152776546551L;
+
+	public MethodDeprecatedInException(String message) {
+		super(message);
+	}
+
+}
